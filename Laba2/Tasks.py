c7367f895383ec8afec1b41f55083334d281fc65
FO2XS <foxxsf@gmail.com>
2021-02-05 18:53:29 +0300

    Update Tasks.py

--- Laba2/Tasks.py
+++ Laba2/Tasks.py
@@ -1,11 +1,12 @@
 
 #Функция вычисления чисел фибоначи. Функция расчитывает следующих count чисел, кроме начальных prev и curr.
+#Kiselev G.L.
 def FiboCalc(prev, curr, count):
     print(prev)
     if count < 0: return 0
     count = count-1
     FiboCalc(curr, prev+curr, count)
+#Example of calling function
+#FiboCalc(0, 1, 10)
 
 
-
-FiboCalc(0, 1, 10)
