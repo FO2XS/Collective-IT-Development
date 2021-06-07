ec75e7010e50d05bf5e5bc8ce4f808b97a3703cf
ArGoN <mr.yayay01@bk.ru>
2021-06-07 20:18:38 +0300

    Minor
    Исправил недочеты

--- Laba8/main.py
+++ Laba8/main.py
@@ -7,7 +7,7 @@ print()
 str = "#03B63A, #FFF, #000, #03B63A"
 Sstr = "ABCDEF, 123, #GHIJKL"
 
-print(re.findall(r'#[0-9A-z]{3,6}', str))
+print(re.findall(r'#[0-9A-z]{6}|#[0-9A-z]{3}', str))
 
 
 print()
@@ -16,7 +16,7 @@ print()
 str = "http://ya.ru/index.html, https://wikipedia.org/"
 Sstr = "ftp://some.server/, https://ru.wikipedia.org/"
 
-result = re.findall(r'http(s:|:)//([a-z|0-9]*.[a-z]*)/', str)
+result = re.findall(r'http(s:|:)//([a-z|0-9]+.[a-z]+)/', str)
 
 for i in result:
     print(i[1])
@@ -28,6 +28,6 @@ str = "test.png, test.jpeg, test.jpg, test.gif"
 Sstr = "test.php, test.exe, ~!@$%.png, <?php test.png ?>"
 
 for i in str.split(", "):
-    result = re.findall(r'^([a-z]*.(png|jpeg|gif|jpg))', i)
+    result = re.findall(r'^([a-z]+.(png|jpeg|gif|jpg))', i)
     if len(result) != 0:
         print(result[0][0])
